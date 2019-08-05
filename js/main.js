93bd55d821faa2590e74dc65321255f480117fe8
Allig8orBlood <Allig8orBlood182@yandex.com>
2019-08-05 21:31:19 +0900

    implement a images tab

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("Hi");
+$(function() {
+   $('.foodGalleryTab > li').on('click', function() {
+      $('.foodGalleryTab > li,.foodGalleryImgs > foodImg').removeClass('active');
+      var tabClass = $(this).attr('class');
+      $(this).addClass('active');
+
+      $('.foodImg').each(function() {
+         if($(this).attr('class').indexOf(tabClass) != -1) {
+            $(this).addClass('active').fadeIn();
+         } else {
+            $(this).hide();
+         }
+      });
+   });
+});
